c467c3575d515298d8f86917b591863eeba98dab
monerica-project <108239499+monerica-project@users.noreply.github.com>
2026-04-18 18:05:11 -0400

    sponsors on porfolio

---
+++ src/MoneroMarketCap.Web/Pages/Shared/_SponsorList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoneroMarketCap.Web.Pages.Shared
+{
+    public class _SponsorListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
