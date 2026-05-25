524880475d80a9318289bdd5d5d292d85d399637
monerica-project <108239499+monerica-project@users.noreply.github.com>
2026-05-25 17:40:00 -0400

    sponsor link

---
+++ src/MoneroMarketCap.Web/Pages/Sponsorship.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoneroMarketCap.Web.Pages
+{
+    public class SponsorshipModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
